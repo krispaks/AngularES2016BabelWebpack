2fb348f156e6622ff2c67a98eeef3cf01945245e
krispaks <krispaks@yahoo.com>
2016-08-22 23:08:17 -0500

    menu

--- app/app.js
+++ app/app.js
@@ -3,13 +3,17 @@ import 'angular-animate';
 import 'angular-aria';
 import 'angular-messages';
 import 'angular-material';
+import 'angular-ui-router';
 
 import AppComponent from './app.component';
 
 export default angular
-    .module('app', ['ngMaterial', 'material.svgAssetsCache'])
+    .module('app', [
+        'ngMaterial'
+        , 'ui.router'
+        , 'material.svgAssetsCache'])
     .component(AppComponent.name, AppComponent.config)
-    .config(($mdThemingProvider)=>{
+    .config(['$mdThemingProvider', ($mdThemingProvider)=>{
         $mdThemingProvider.theme('default')
             .primaryPalette('orange');
-    });
+    }]);
---
+++ app/components/home/home.module.js
@@ -0,0 +1,11 @@
+import angular from 'angular';
+import 'angular-animate';
+import 'angular-aria';
+import 'angular-messages';
+import 'angular-material';
+import 'angular-ui-router';
+
+export default angular.module('home', [
+    'ngMaterial'
+    , 'ui.router'
+    , 'material.svgAssetsCache']);
